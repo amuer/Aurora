b6c4b04fb86df3428d31656310e70c15613e0417
amuer <wangqiang4330@gmail.com>
2015-07-23 15:42:46 +0800

    基础消息处理类

--- src/main/java/com/amuer/aurora/base/logichandler/BaseMessageHandler.java
+++ src/main/java/com/amuer/aurora/base/logichandler/BaseMessageHandler.java
@@ -1,10 +1,20 @@
 package com.amuer.aurora.base.logichandler;
 
 import com.amuer.aurora.base.msghandler.IMsgHandler;
+import com.amuer.aurora.base.users.IUser;
 
 /**
  * Created by wangqiang on 2015/7/23.
  */
 public class BaseMessageHandler implements IMsgHandler
 {
+    public int handle(Object msg)
+    {
+        return 0;
+    }
+
+    public int handle(IUser user, Object msg)
+    {
+        return 0;
+    }
 }
